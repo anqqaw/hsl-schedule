e35ed2879a83129a19ff2c4f8717cb074286aaad
Aarni Klemetti <aarniklemetti@Aarnis-MacBook-Pro.local>
2024-05-28 15:05:16 +0300

    change: switch theme button switches in order

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 function App() {
   const [schedule, setSchedule] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [theme, setTheme] = useState('darkPurple');
+  const [themeIndex, setThemeIndex] = useState(0);
 
   useEffect(() => {
     const ENDPOINT = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
-    const CODE = process.env.REACT_APP_APIKEY; 
+    const CODE = process.env.REACT_APP_APIKEY;
 
     const query = `{
       stop(id: "HSL:2112401") {
@@ -48,21 +48,15 @@ function App() {
       });
   }, []);
 
-  const themeClasses = {
-    darkPurple: 'bg-gradient-to-r from-purple-900 via-purple-800 to-purple-900 text-white shadow-lg',
-    red: 'bg-gradient-to-r from-red-700 via-red-600 to-red-700 text-white shadow-lg',
-    green: 'bg-gradient-to-r from-green-700 via-green-600 to-green-700 text-white shadow-lg',
-    blue: 'bg-gradient-to-r from-blue-800 via-blue-700 to-blue-800 text-white shadow-lg',
-  };
+  const themeClasses = [
+    'bg-gradient-to-r from-purple-900 via-purple-800 to-purple-900 text-white shadow-lg',
+    'bg-gradient-to-r from-red-700 via-red-600 to-red-700 text-white shadow-lg',
+    'bg-gradient-to-r from-green-700 via-green-600 to-green-700 text-white shadow-lg',
+    'bg-gradient-to-r from-blue-800 via-blue-700 to-blue-800 text-white shadow-lg',
+  ];
 
-  const generateRandomTheme = () => {
-    const themes = Object.keys(themeClasses);
-    const randomIndex = Math.floor(Math.random() * themes.length);
-    return themes[randomIndex];
-  };
-
-  const changeTheme = () => {
-    setTheme(generateRandomTheme());
+  const nextTheme = () => {
+    setThemeIndex((prevIndex) => (prevIndex + 1) % themeClasses.length);
   };
 
   if (loading) {
@@ -74,7 +68,7 @@ function App() {
   }
 
   return (
-    <div className={`p-4 ${themeClasses[theme]} min-h-screen flex flex-col items-center`}>
+    <div className={`p-4 ${themeClasses[themeIndex]} min-h-screen flex flex-col items-center`}>
       <h1 className="text-3xl font-bold mb-4 drop-shadow-lg shadow-black text-center bg-gradient-to-r from-gray-800 to-gray-900 bg-clip-text text-transparent">
         Leppävaaran asema
       </h1>
@@ -92,10 +86,10 @@ function App() {
         </tbody>
       </table>
       <button 
-        onClick={changeTheme}
+        onClick={nextTheme}
         className="mt-4 py-2 px-4 bg-gray-800 text-white rounded-lg shadow-md hover:bg-gray-700 transition duration-300"
       >
-        Switch Theme
+        Vaihda Teema
       </button>
     </div>
   );
