fbe02568e9812b3cf57020fa8d25477d1b9fd344
Aarni Klemetti <aarniklemetti@Aarnis-MacBook-Pro.local>
2024-05-28 15:03:37 +0300

    Added switch theme button

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 function App() {
   const [schedule, setSchedule] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [theme, setTheme] = useState('darkPurple');
 
   useEffect(() => {
     const ENDPOINT = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
     const CODE = process.env.REACT_APP_APIKEY; 
-  
+
     const query = `{
       stop(id: "HSL:2112401") {
         name
@@ -26,7 +27,7 @@ function App() {
         }
       }
     }`;
-  
+
     fetch(ENDPOINT, {
       method: 'POST',
       headers: {
@@ -53,8 +54,16 @@ function App() {
     green: 'bg-gradient-to-r from-green-700 via-green-600 to-green-700 text-white shadow-lg',
     blue: 'bg-gradient-to-r from-blue-800 via-blue-700 to-blue-800 text-white shadow-lg',
   };
-  
-  const selectedTheme = themeClasses.darkPurple; 
+
+  const generateRandomTheme = () => {
+    const themes = Object.keys(themeClasses);
+    const randomIndex = Math.floor(Math.random() * themes.length);
+    return themes[randomIndex];
+  };
+
+  const changeTheme = () => {
+    setTheme(generateRandomTheme());
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -65,7 +74,7 @@ function App() {
   }
 
   return (
-    <div className={`p-4 ${selectedTheme} min-h-screen flex flex-col items-center`}>
+    <div className={`p-4 ${themeClasses[theme]} min-h-screen flex flex-col items-center`}>
       <h1 className="text-3xl font-bold mb-4 drop-shadow-lg shadow-black text-center bg-gradient-to-r from-gray-800 to-gray-900 bg-clip-text text-transparent">
         Leppävaaran asema
       </h1>
@@ -82,6 +91,12 @@ function App() {
           ))}
         </tbody>
       </table>
+      <button 
+        onClick={changeTheme}
+        className="mt-4 py-2 px-4 bg-gray-800 text-white rounded-lg shadow-md hover:bg-gray-700 transition duration-300"
+      >
+        Switch Theme
+      </button>
     </div>
   );
 }
